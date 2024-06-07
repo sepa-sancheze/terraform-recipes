83cd304470571dd295984f5d28b54e42d7623aa1
José Sánchez <jose.venados6@gmail.com>
2024-06-07 07:41:59 -0600

    Create EC2 instance with Public IP

--- aws/ec2/2-ec2_instance.tf
+++ aws/ec2/2-ec2_instance.tf
@@ -2,37 +2,25 @@ resource "aws_vpc" "tf_vpc" {
   cidr_block = "172.16.0.0/16"
 
   tags = {
-    Name = "tf_vpc-example"
+    Name = "tf_vpc"
   }
 }
 
 resource "aws_subnet" "tf_subnet" {
-  vpc_id            = aws_vpc.tf_vpc.id
   cidr_block        = "172.16.0.0/24"
   availability_zone = "us-east-1a"
+  vpc_id            = aws_vpc.tf_vpc.id
 
   tags = {
-    Name = "tf_subnet-example"
-  }
-}
-
-resource "aws_network_interface" "tf_network-interface" {
-  subnet_id   = aws_subnet.tf_subnet.id
-  private_ips = ["172.16.0.100"]
-
-  tags = {
-    Name = "tf_primary-network-interface"
+    Name = "tf_subnet"
   }
 }
 
 resource "aws_instance" "tf_ec2-instance" {
-  ami           = "ami-04b70fa74e45c3917"
-  instance_type = "t2.micro"
-
-  network_interface {
-    network_interface_id = aws_network_interface.tf_network-interface.id
-    device_index         = 0
-  }
+  ami                         = "ami-04b70fa74e45c3917"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.tf_subnet.id
 
   credit_specification {
     cpu_credits = "unlimited"
