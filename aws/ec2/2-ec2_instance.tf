dededf6ec2dbf3e17652eb3332734bfa295532c6
José Sánchez <jose.venados6@gmail.com>
2024-06-10 19:27:28 -0600

    From scratch

--- aws/ec2/2-ec2_instance.tf
+++ aws/ec2/2-ec2_instance.tf
@@ -1,32 +1,16 @@
 resource "aws_vpc" "tf_vpc" {
-  cidr_block = "172.16.0.0/16"
+  cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "tf_vpc"
+    Name = "production_vpc"
   }
 }
 
-resource "aws_subnet" "tf_subnet" {
-  cidr_block        = "172.16.0.0/24"
-  availability_zone = "us-east-1a"
-  vpc_id            = aws_vpc.tf_vpc.id
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.tf_vpc.id
+  cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "tf_subnet"
-  }
-}
-
-resource "aws_instance" "tf_ec2-instance" {
-  ami                         = "ami-04b70fa74e45c3917"
-  instance_type               = "t2.micro"
-  associate_public_ip_address = true
-  subnet_id                   = aws_subnet.tf_subnet.id
-
-  credit_specification {
-    cpu_credits = "unlimited"
-  }
-
-  tags = {
-    Name = "tf_ec2-example"
+    Name = "production_subnet"
   }
 }
